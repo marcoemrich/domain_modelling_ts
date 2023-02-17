c60bcd6b5cba2484ceb35189f88b12058bef1cfe
marcoemrich <marco.emrich@codecentric.de>
2023-02-17 07:04:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# domain_modelling_ts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-3nw73d)
---
+++ index.ts
@@ -0,0 +1,11 @@
+// Import stylesheets
+import './style.css';
+
+type X = number;
+
+const x: X = '4';
+
+console.log('foo');
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
